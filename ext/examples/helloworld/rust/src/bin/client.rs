9a935625f013af4dbf2f419229c6cc30e973e266
ForGoodTech <ForGoodTech@surestinfo.com>
2025-11-10 01:52:33 -0500

    rust helloworld

--- ext/examples/helloworld/rust/src/bin/client.rs
+++ ext/examples/helloworld/rust/src/bin/client.rs
@@ -1,40 +1,61 @@
-use std::time::Duration;
-
 use tokio::net::UnixStream;
-use tonic::transport::{Channel, Endpoint, Uri};
-use tonic::Request;
+use tonic::transport::{
+    Channel,
+    Endpoint,
+    Uri,
+};
 use tower::service_fn;
 
-pub mod helloworld {
+pub mod helloworld
+{
     tonic::include_proto!("helloworld");
 }
 
-use helloworld::greeter_client::GreeterClient;
-use helloworld::HelloRequest;
+use helloworld::{
+    greeter_client::GreeterClient,
+    HelloRequest,
+};
 
-async fn connect_via_unix_socket(path: &str) -> Result<Channel, Box<dyn std::error::Error>> {
+async fn connect_via_unix_socket(path: impl Into<String>) -> Result<Channel, Box<dyn std::error::Error>>
+{
+    let path = path.into();
+
     let endpoint = Endpoint::try_from("http://[::]:50051")?;
+
     let channel = endpoint
-        .connect_with_connector(service_fn(move |_: Uri| {
-            let path = path.to_owned();
-            async move { UnixStream::connect(path).await }
+        .connect_with_connector(service_fn(move |_: Uri|
+        {
+            let path = path.clone();
+
+            async move
+            {
+                UnixStream::connect(path).await
+            }
         }))
         .await?;
+
     Ok(channel)
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let socket_path = "/tmp/helloworld.sock";
-    let mut client = GreeterClient::new(connect_via_unix_socket(socket_path).await?);
+async fn main() -> Result<(), Box<dyn std::error::Error>>
+{
+    // Must match the server's "Server listening on /tmp/helloworld.sock"
+    let channel = connect_via_unix_socket("/tmp/helloworld.sock").await?;
 
-    let request = Request::new(HelloRequest {
-        name: "world".into(),
-    });
+    let mut client = GreeterClient::new(channel);
 
-    let response =
-        tokio::time::timeout(Duration::from_secs(1), client.say_hello(request)).await??;
-    println!("Greeting: {}", response.into_inner().message);
+    let request = tonic::Request::new(
+        HelloRequest
+        {
+            name: "Codex".to_string(),
+        }
+    );
+
+    let response = client.say_hello(request).await?;
+
+    println!("Server replied: {}", response.into_inner().message);
 
     Ok(())
 }
+
