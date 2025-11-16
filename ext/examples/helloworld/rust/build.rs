740bbf9bbf7a13eacb4331b70d620ba18f40f802
ForGoodTech <88217240+ForGoodTech@users.noreply.github.com>
2025-11-16 00:45:06 -0500

    gRPC not needed for now

--- ext/examples/helloworld/rust/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    tonic_build::configure()
-        .build_server(true)
-        .build_client(true)
-        .compile(&["../helloworld.proto"], &["../"])
-        .expect("failed to compile protos");
-}
